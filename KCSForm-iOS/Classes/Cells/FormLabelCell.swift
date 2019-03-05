49e74bd59128e8b342c921c5b60bc7f24327e6c5
Sean Batson <3752237+adozenlines@users.noreply.github.com>
2019-03-04 20:29:36 -0500

    Update FormLabelCell.swift

--- KCSForm-iOS/Classes/Cells/FormLabelCell.swift
+++ KCSForm-iOS/Classes/Cells/FormLabelCell.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-class FormLabelCell: UICollectionViewCell, FormCell {
+public class FormLabelCell: UICollectionViewCell, FormCell {
     
     public class Data: FormCellData {
         let text: NSAttributedString
